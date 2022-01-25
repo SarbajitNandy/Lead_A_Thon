d430b830ea8822fbc6b560f38db44db157bad7fa
Sarbajit Nandy <sarbajitnandey@gmail.com>
2022-01-25 13:23:06 +0530

    Update fetchFromWeb.js

--- fetchFromWeb.js
+++ fetchFromWeb.js
@@ -1,4 +1,4 @@
-const request = require("request-promise")
+// const request = require("request-promise")
 const fetch = require("node-fetch")
 const cheerio = require("cheerio")
 const ChessMove = require("./model");
